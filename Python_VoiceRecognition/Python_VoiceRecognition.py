37deb56d7a7b5a5f203074832b8ab0dd3fa8e267
Supawich@Laptop29 <supawichza@gmail.com>
2021-11-02 00:03:55 +0000

    Completed the voice recognition application, with try except for microphone index and invalid audio input.

--- Python_VoiceRecognition/Python_VoiceRecognition.py
+++ Python_VoiceRecognition/Python_VoiceRecognition.py
@@ -2,13 +2,15 @@
 
 #My first voice recognition program
 import speech_recognition as sr
+import os
 counter = 0
 recognizer_instance = sr.Recognizer()
 
-#test_data = sr.AudioFile("test_data.wav")
+test_data = sr.AudioFile("test_data.wav")
 #1234 hello world
-test_data = sr.AudioFile("audio_files_jackhammer.wav")
+# test_data = sr.AudioFile("audio_files_jackhammer.wav")
 #the snail smell of old gear vendors.
+print(os.getcwd())
 with test_data as source:
     recognizer_instance.adjust_for_ambient_noise(source,duration = 0.5)
 
@@ -23,13 +25,13 @@ microphone = sr.Microphone()
 
 
 
-#Check a list of the microphone
-#for x in sr.Microphone.list_microphone_names():
-#    print(counter,x)
-#    counter+=1
+# Check a list of the microphone
+for x in sr.Microphone.list_microphone_names():
+   print(counter,x)
+   counter+=1
 
-lst_mic = sr.Microphone.list_microphone_names()
-mic = sr.Microphone(device_index=1)
+
+
 
 #Test first microphone interpreter
 #print("Please speak into the microphone.")
@@ -43,17 +45,22 @@ time = 0
 
 while True:
     try:
-        with mic as source:
-            print("Please speak into the microphone")
-            audio = recognizer_instance.listen(source)
-            for _ in range(31):
+        try:
+            mic_index = input("What is the index of the microphone, you would like to use?")
+            mic = sr.Microphone(device_index=1)
+            with mic as source:
+                print("Please speak into the microphone")
+                audio = recognizer_instance.listen(source)
+            
                 recognizer_instance.adjust_for_ambient_noise(source,duration = time)
+                print(recognizer_instance.recognize_google((audio)))
 
-                print("time = {0} output = {1}".format(time,recognizer_instance.recognize_google(audio)))
-                time = time + 0.1
 
 
     
-    except :
-        print("Invalid input to microphone, please try again")
+        except :
+            print("Invalid input to microphone, please try again")
 
+    except:
+        print("Invalid index of microphone, please try again")
+    
